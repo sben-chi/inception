f00b33386c92ee1c144051b28d0c2bcd9c1e2e9f
root <root@DESKTOP-6Q4DTOP>
2023-05-18 11:27:57 +0100

    .

--- tests/test_m/files/run.sh
+++ tests/test_m/files/run.sh
@@ -2,8 +2,11 @@
 
 /etc/init.d/mysql start;
 
-mysql -u root -e "CREATE DATABASE wordpress;";
+mysql -u root -e "CREATE DATABASE wordpress ; \
+				CREATE USER sben-chi@localhost IDENTIFIED BY ' '; \
+				GRANT ALL PRIVILEGES ON wordpress.* TO sben-chi@localhost ;\
+				FLUSH PRIVILEGES;";
 
-mysql -u root;
+ping localhost;
 
 #tail -f;
